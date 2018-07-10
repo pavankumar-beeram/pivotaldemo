b61b7740a1e086c606412821180147c59b77a2f3
Rohit Sai <rohit.sai@movimentogroup.com>
2018-07-10 21:43:33 +0530

    first commit

---
+++ src/test/java/com/pivot/PivotaldemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pivot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PivotaldemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
